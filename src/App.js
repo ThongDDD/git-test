bbc9a6a0c1cb5044d4934001316ce0d808772666
ThongD <thong.do@gumiviet.com>
2020-08-06 11:25:27 +0700

    Revert "Revert "Revert "Revert "Revert "set people"""""
    This reverts commit 4a2a4d47799923715f3530d73cd8a654acbaee20.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const [people, setPeople] = useState([
-    {id: 1, name: 'David', age: '25', job: 'dev'},
-    {id: 2, name: 'Jack', age: '30', job: 'architecture'},
-    {id: 3, name: 'John', age: '27', job: 'musician'},
-  ])
-
   return (
     <div className="App">
       <h3>This is my first line of codes in this project</h3>
-      <table>
-        <thead>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Age</th>
-          <th>Job</th>
-        </thead>
-        <tbody>
-          
-        </tbody>
-      </table>
     </div>
   );
 }
